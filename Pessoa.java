6f63523ad4c3eccf707baa70a2bd81b11b19e6c5
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-06 11:08:53 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.interfacegrafica2;
+
+/**
+ *
+ * @author cadua
+ */
+class Pessoa {
+    String nome;
+    int idade;
+    String sexo;
+    double altura;
+    
+    
+    Pessoa(String nome, int idade, String sexo, double altura){
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+    this.altura = altura;
+    }
+}
